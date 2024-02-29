d855ccbabaab7b6629f511268a3d8ce52c2596f8
Dmitry Osipov <dimaosipov00@gmail.com>
2024-02-29 18:18:56 +0300

    Created the first Spring MVC application. Created a controller class, shardcoded the view, studied RequestMapping and Controller annotations

---
+++ src/main/java/com/osipov/spring/mvc/MyController.java
@@ -0,0 +1,15 @@
+package com.osipov.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+    @RequestMapping("/")  // Аннотация используется для маппинга адреса, который мы будем вводить в браузере, с
+    // методом. Т.е. при вводе в браузере "/" будет отображаться view с названием first-view.
+    public String showFirstView() {  // Сам метод может называться абсолютно как угодно, но лучше указывать осмысленные
+        // названия. У метода могут быть параметры. Также и return-type может быть другим, но наиболее часто используют
+        // именно String для возврата названия view.
+        return "first-view";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dimao
+  Date: 29.02.2024
+  Time: 17:59
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+</head>
+<body>
+    <h2>I wish you luck in Spring MVC learning</h2>
+</body>
+</html>
